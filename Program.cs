489e106e4655443e212200a61a581ecced3e49c8
Isaline FOISSEY <126682144+isalinefsy@users.noreply.github.com>
2025-12-24 13:11:08 +0000

    changes done to enable deployment on prod

--- Program.cs
+++ Program.cs
@@ -3,22 +3,30 @@ using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
+builder.Services.AddDbContext<MyDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+
 if(builder.Environment.IsDevelopment())
 {
-    builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
     builder.Services.AddDistributedMemoryCache();
 }
-// else
-// {
-//     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-//         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-//     builder.Services.AddStackExchangeRedisCache(options =>
-//     {
-//     options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-//     options.InstanceName = "SampleInstance";
-//     });
-// }
+else
+{
+    var redisConn = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+    if (!string.IsNullOrWhiteSpace(redisConn))
+    {
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = redisConn;
+            options.InstanceName = "SampleInstance";
+        });
+    }
+    else
+    {
+        // Fallback when Redis connection string isn't present
+        builder.Services.AddDistributedMemoryCache();
+    }
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
